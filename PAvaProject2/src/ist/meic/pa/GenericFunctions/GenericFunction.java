333e02105d28c5b58216c79d7c16bbaa1d752350
Gonçalo Castilho <goncalocastilho@hotmail.com>
2016-04-21 11:42:04 +0100

    Comentada a classe com o que temos de fazer

---
+++ PAvaProject2/src/ist/meic/pa/GenericFunctions/GenericFunction.java
@@ -0,0 +1,15 @@
+package ist.meic.pa.GenericFunctions;
+
+public class GenericFunction {
+//	A funçao generica tem de conter todas as especificações do metodo generico
+//	Para escolher o metodo a executar(effective Method) temos de fazer os seguintes passos:
+//	-Selecionar os metodos adequados aos argumentos passados(cujos argumentos façam "wrap")
+//	-Ordenar os metodos por ordem de precedencia do mais especifico para o menos especifico
+//	-Combinar os metodos aplicaveis(1 primário , multiplos auxiliares) e produzir o effective method
+//	
+//	usando como argumentos a funçao generica original e os argumentos da chamada a funçao
+//	Se existir um Effective Method executamo-lo, caso contrário, chamamos "no-applicable-method"
+
+	
+	
+}
