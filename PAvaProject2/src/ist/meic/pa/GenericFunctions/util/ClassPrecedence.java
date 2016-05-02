2565dc6057da574ebca0ba8429fdac5b9ec1c542
Gonçalo Castilho <goncalocastilho@hotmail.com>
2016-05-02 17:30:04 +0100

    Precedencia de classes implementada com BFS
    Dizer na apresentaçao a razao da nossa escolha

--- PAvaProject2/src/ist/meic/pa/GenericFunctions/util/ClassPrecedence.java
+++ PAvaProject2/src/ist/meic/pa/GenericFunctions/util/ClassPrecedence.java
@@ -18,26 +18,58 @@ public class ClassPrecedence{
 	 * @param clazz
 	 * @return
 	 */
+//	public static List<Class> getSuperClasses(Class clazz) {
+//	  List<Class> clazzList = new ArrayList<Class>();
+//	  Class superclazz = clazz.getSuperclass();
+//	  clazzList.add(clazz);
+//	  Class[] interfaces= clazz.getInterfaces();
+//	  for(Class c : interfaces){
+//		  clazzList.add(c);
+//	  }
+//	  while (superclazz != null) {   
+//		clazzList.add(superclazz);
+//		interfaces= superclazz.getInterfaces();
+//		for(Class c : interfaces){
+//			if(!clazzList.contains(c)){
+//				clazzList.add(c);
+//			}
+//			
+//		}
+//		clazz = superclazz;
+//		superclazz = clazz.getSuperclass();
+//	  }
+//	  return clazzList;
+//	}
+	
 	public static List<Class> getSuperClasses(Class clazz) {
-	  List<Class> clazzList = new ArrayList<Class>();
-	  Class superclazz = clazz.getSuperclass();
-	  clazzList.add(clazz);
-	  Class[] interfaces= clazz.getInterfaces();
-	  for(Class c : interfaces){
-		  clazzList.add(c);
-	  }
-	  while (superclazz != null) {   
-		clazzList.add(superclazz);
-		interfaces= superclazz.getInterfaces();
+		int i=1;
+		List<Class> clazzList = new ArrayList<Class>();
+		clazzList.add(clazz);
+		clazzList.add(clazz.getSuperclass());
+		Class[] interfaces= clazz.getInterfaces();
 		for(Class c : interfaces){
-			if(!clazzList.contains(c)){
-				clazzList.add(c);
-			}
-			
+		 clazzList.add(c);
 		}
-		clazz = superclazz;
-		superclazz = clazz.getSuperclass();
-	  }
-	  return clazzList;
+		if(clazzList.size()==1){
+			return clazzList;
+		}
+		while (true) { 
+			clazz=clazzList.get(i);
+			if(clazz.getSuperclass()!=null){
+				clazzList.add(clazz.getSuperclass());
+			}
+			interfaces= clazz.getInterfaces();
+			for(Class c : interfaces){
+				if(!clazzList.contains(c)){
+					clazzList.add(c);
+				}
+				
+			}
+			i++;
+			if(i== clazzList.size()-1){
+				break;
+			}
+		 }
+		return clazzList;
 	}
 }
